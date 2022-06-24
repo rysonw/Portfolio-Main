1f4996aa336d13988985844f201dd1f6eafb085b
rysonw <rysonplays@gmail.com>
2022-06-24 08:19:37 -1000

    Footer Changes

--- src/components/Home/FooterSocial.jsx
+++ src/components/Home/FooterSocial.jsx
@@ -1,26 +1,23 @@
-import { BiEnvelopeOpen } from "react-icons/bi";
+import { BsInstagram } from "react-icons/bi";
 import { BsGithub } from "react-icons/bs";
 import { FaLinkedinIn } from "react-icons/fa";
 const FooterSocial = () => {
   return (
     <div className="flex items-center gap-x-5 my-6 justify-center">
       <a
-        href="#!"
+        href="https://www.linkedin.com/in/ryson-wong-a85980215/"
         className="bg-custom-blue hover:bg-custom-purple rounded-full p-1 w-[3.2rem] h-[3.2rem] flex justify-center items-center duration-700 ease-in-out transform hover:scale-[1.2]"
       >
         <FaLinkedinIn className="text-2xl text-white " />
       </a>
       <a
-        href="#!"
+        href="https://github.com/rysonw"
         className="bg-custom-blue hover:bg-custom-purple rounded-full p-1 w-[3.2rem] h-[3.2rem] flex justify-center items-center duration-700 ease-in-out transform hover:scale-[1.2]"
       >
         <BsGithub className="text-2xl text-white " />
       </a>
-      <a
-        href="#!"
-        className="bg-custom-blue hover:bg-custom-purple rounded-full p-1 w-[3.2rem] h-[3.2rem] flex justify-center items-center duration-700 ease-in-out transform hover:scale-[1.2]"
-      >
-        <BiEnvelopeOpen className="text-2xl text-white " />
+      <a href="https://www.instagram.com/dawongdong/">
+        <BsInstagram className=" text-3xl hover:text-[#cf3b9d] transform duration-700 ease-in-out hover:rotate-[360deg]" />
       </a>
     </div>
   );
