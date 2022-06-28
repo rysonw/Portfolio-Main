5e77cc161e7b3a26b465ba330c31abd421c2c067
rysonw <rysonplays@gmail.com>
2022-06-28 01:27:12 -1000

    Apperance Changes

--- src/components/Home/Education.jsx
+++ src/components/Home/Education.jsx
@@ -6,13 +6,13 @@ const Education = () => {
         Education
       </h3>
       <div className="flex justify-left gap-x-6 ml-0 md:ml-10">
-        <IoSchoolOutline className="text-6xl text-primary" />
+        <link rel="icon" href="./CCAF.jpg" className="text-6xl text-primary" />
         <div className="text-left">
           <h6 className="text-xl font-medium font-primary text-gray-900">
             Community College of the Air Force
           </h6>
           <small className="text-base font-medium font-primary">
-            2020 - 2022
+            2019 - 2022
           </small>
           <p className="mt-2 text-xl  font-primary">Associates in Electronic Systems Technology</p>
         </div>
