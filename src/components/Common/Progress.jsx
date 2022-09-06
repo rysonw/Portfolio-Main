16170b9e0a72cd41d66ddce5403e1af41b1ebd9c
wongr <wongryson36@gmail.com>
2022-09-05 23:46:00 -1000

    Changes to WIP section

--- src/components/Common/Progress.jsx
+++ src/components/Common/Progress.jsx
@@ -20,7 +20,7 @@ const Progress = ({ done, title }) => {
       <h3 className="mb-2 text-[1.08rem] font-primary text-left">{title}</h3>
       <div className="bg-primary rounded-[20px] relative h-3 w-full progress-line">
         <div
-          className={`bg-primary rounded-[20px] flex justify-center items-center h-full w-0 opacity-0 transform ease duration-1000 delay-300 animated absolute after:absolute ${result} after:top-[-28px] after:right-0`}
+          className={`bg-primary rounded-[20px] flex justify-center items-center h-full w-0 opacity-0 transform ease duration-1000 delay-300 `}
           style={style}
         ></div>
       </div>
